16e7f15bcdec0a1bd180dbd8fa37ad318f794555
satish_sinha <satishk.sinha@digitalindia.gov.in>
2024-08-26 19:07:49 +0530

    changes for user dashboard

--- frontend/src/components/User/Header.js
+++ frontend/src/components/User/Header.js
@@ -6,7 +6,7 @@ import './Dashboard.css';
 const Header = () => {
   return (
     <header className="dashboard__header">
-      <h1>Dashboard</h1>
+      <h1>Welcome </h1>
       <div className="dashboard__search-container">
         <input type="text" className="dashboard__search-box" placeholder="Search..." />
         <button className="dashboard__search-button">Search</button>
